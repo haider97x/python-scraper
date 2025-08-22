bd35f6ac1f0397c62a10282294ac0257e9bb2e0a
haider97x <haider.real97@gmail.com>
2025-08-22 21:18:27 +0500

    Add hello.py: My first Python script with greeting and math

---
+++ hello.py
@@ -0,0 +1,2 @@
+Name= input("Enter your name: ")
+print(f"{Name} is a guy you really dont wanna talk about honestly he has no friends , he has many  passions yet he fails each but sure he will strive")
