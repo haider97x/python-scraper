fb6892969fa0a9190b97fb2211830ffefd2e42a6
haider97x <haider.real97@gmail.com>
2025-08-25 21:22:05 +0500

    Add basics.py: Practice variables, data types, print formatting, and arithmetic

---
+++ basics.py
@@ -0,0 +1,9 @@
+name= "Haider"
+age= 20
+height= 6
+student= True
+print(f"His Name is : {name} and he is aged {age} years old, he is about {height} tall and yet he is a student? {student}")
+years_until_30 = 30 - age
+height_squared = height ** 2
+print(f"{name}, in {years_until_30} years, you’ll be 30!")
+print(f"Your height squared is {height_squared}  feet")
